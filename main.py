4370baead33b8b488ebafc882766996b5c2c50ab
robinfire110 <aevillas@uncg.edu>
2022-07-05 22:48:13 +0000

    Added SQL Database

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 import requests
 #import tkinter
 import json
-#import sqlalchemy
+import sqlalchemy as db
 import random
+import time
+import pandas as pd
+import re
 
 #API
 def get_data():
@@ -15,6 +18,7 @@ def get_data():
 def replace_invalid_characters(string):
     string = string.replace("&#039;", "\'")
     string = string.replace("&quot;", "\"")
+    string = string.replace("&amp;", "&")
     return string
 
 #Display Question and Answers (returns list of answers)
@@ -51,6 +55,25 @@ def check_answer(list_of_answers, list_of_questions):
 #Run
 data = get_data()
 list_of_questions = data["results"]
+start_time = time.time()
+
+#Setup Database
+engine = db.create_engine('sqlite:///data_base_name.db')
+if not engine.has_table("game"):
+    engine.execute("CREATE TABLE game (id INT(8) NOT NULL default 0, Q1 BOOLEAN default 0, Q2 BOOLEAN default 0, Q3 BOOLEAN default 0, Q4 BOOLEAN default 0, Q5 BOOLEAN default 0, Q6 BOOLEAN default 0, Q7 BOOLEAN default 0, Q8 BOOLEAN default 0, Q9 BOOLEAN default 0, Q10 BOOLEAN default 0, Total_Time FLOAT(24) default 0, Percentage FLOAT(24) default 0, Total Score FLOAT(24) default 0);")
+    engine.execute(f"INSERT INTO game (id) VALUES (1);")
+else:
+    #Get Row number
+    row_num = engine.execute("SELECT COUNT(*) from game").fetchall()
+    temp = re.findall(r'\d+', str(row_num[0]))
+    res = list(map(int, temp))
+    row_num = res[0]+1
+    engine.execute(f"INSERT INTO game (id) VALUES ({row_num});")
+
+#Print Database
+#print(engine.execute("SELECT * FROM game").fetchall())
+
+#Run loop for game
 for index in range(0, len(list_of_questions)):
     #Replace Broken Characters
     list_of_questions[index]["question"] = replace_invalid_characters(list_of_questions[index]["question"])
@@ -63,3 +86,5 @@ for index in range(0, len(list_of_questions)):
 
     #Get Input
     check_answer(list_of_answers, list_of_questions)
+
+#print("Total Time: " + str(time.time()-start_time))
